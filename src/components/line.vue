556d03a1c1184de0708366361b45e292b53384f1
seogi1004 <seogi777@gmail.com>
2018-07-03 22:02:05 +0900

    기본 개발환경 구성!!!

--- src/components/line.vue
+++ src/components/line.vue
@@ -3,32 +3,19 @@
 </template>
 <script>
     import props from './mixins/props.js'
+    import watch from './mixins/watch.js'
     import methods from './mixins/methods.js'
     import created from './mixins/created.js'
     import mounted from './mixins/mounted.js'
 
     export default {
         name: 'graph-line',
-        mixins: [ props, methods, created, mounted ],
+        mixins: [ props, watch, methods, created, mounted ],
         props: {
             shape: {
                 type: String,
                 required: false,
                 default: 'curve'
-            }
-        },
-        watch: {
-            width: function(newVal, oldVal) {
-                this.chart.setSize(newVal, this.height);
-                this.chart.render()
-            },
-            height: function(newVal, oldVal) {
-                this.chart.setSize(this.width, newVal);
-                this.chart.render()
-            },
-            values: function (newVal, oldVal) { // watch it
-                this.chart.axis(0).update(this.convertToData(newVal));
-                this.chart.render();
             }
         },
         beforeMount: function() {
---
+++ src/components/mixins/watch.js
@@ -0,0 +1,16 @@
+export default {
+    watch: {
+        width: function(newVal, oldVal) {
+            this.chart.setSize(newVal, this.height);
+            this.chart.render()
+        },
+        height: function(newVal, oldVal) {
+            this.chart.setSize(this.width, newVal);
+            this.chart.render()
+        },
+        values: function (newVal, oldVal) { // watch it
+            this.chart.axis(0).update(this.convertToData(newVal));
+            this.chart.render();
+        }
+    },
+}
