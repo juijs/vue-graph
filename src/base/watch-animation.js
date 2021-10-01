2644acf876087e4e56d737c7231db9feb03ed905
alvin.hhh <alvin@jennifersoft.com>
2021-10-01 14:29:32 +0900

    watch를 vue3 버전에 맞게 마이그레이션

--- src/base/watch-animation.js
+++ src/base/watch-animation.js
@@ -1,16 +1,19 @@
 export default {
     watch: {
-        renderStop: function(newVal, oldVal) {
-            if(newVal) {
-                this.animation.stop();
-            } else {
-                const self = this;
-                this.animation.run(function(runningTime) {
-                    if(typeof(self.renderHandler) == "function") {
-                        self.renderHandler.call(this, runningTime);
-                    }
-                });
-            }
+        renderStop: {
+            handler(newVal, oldVal) {
+                if(newVal) {
+                    this.animation.stop();
+                } else {
+                    const self = this;
+                    this.animation.run(function(runningTime) {
+                        if(typeof(self.renderHandler) == "function") {
+                            self.renderHandler.call(this, runningTime);
+                        }
+                    });
+                }
+            },
+            deep: false
         }
     }
 }
--- src/base/watch.js
+++ src/base/watch.js
@@ -1,46 +1,70 @@
 export default {
     watch: {
-        width: function(newVal, oldVal) {
-            this.chart.setSize(newVal, this.height);
-            this.chart.render()
+        width: {
+            handler(newVal, oldVal) {
+                this.chart.setSize(newVal, this.height);
+                this.chart.render()
+            },
+            deep: false
         },
-        height: function(newVal, oldVal) {
-            this.chart.setSize(this.width, newVal);
-            this.chart.render()
+        height: {
+            handler(newVal, oldVal) {
+                this.chart.setSize(this.width, newVal);
+                this.chart.render()
+            },
+            deep: false
         },
-        labels: function(newVal, oldVal) { // watch it
-            this.chart.axis(0).set(this.axisReverse ? 'y' : 'x', { domain: newVal });
-            this.chart.render();
+        labels: {
+            handler(newVal, oldVal) { // watch it
+                this.chart.axis(0).set(this.axisReverse ? 'y' : 'x', { domain: newVal });
+                this.chart.render();
+            }, 
+            deep: false
         },
-        values: function(newVal, oldVal) { // watch it
-            this.chart.axis(0).update(this.convertToData(newVal));
-            this.chart.render();
+        values: {
+            handler(newVal, oldVal) { // watch it
+                this.chart.axis(0).update(this.convertToData(newVal));
+                this.chart.render();
+            },
+            deep: false
         },
-        axisMin: function(newVal, oldVal) {
-            this.chart.axis(0).set(this.axisReverse ? 'x' : 'y', { domain: [ newVal, this.axisMax ] });
-            this.chart.render();
+        axisMin: {
+            handler(newVal, oldVal) {
+                this.chart.axis(0).set(this.axisReverse ? 'x' : 'y', { domain: [ newVal, this.axisMax ] });
+                this.chart.render();
+            },
+            deep: false
         },
-        axisMax: function(newVal, oldVal) {
-            this.chart.axis(0).set(this.axisReverse ? 'x' : 'y', { domain: [ this.axisMin, newVal ] });
-            this.chart.render();
+        axisMax: {
+            handler(newVal, oldVal) {
+                this.chart.axis(0).set(this.axisReverse ? 'x' : 'y', { domain: [ this.axisMin, newVal ] });
+                this.chart.render();
+            },
+            deep: false
         },
-        focusStart: function(newVal, oldVal) {
-            if(newVal == oldVal) return;
-            this.brushes.forEach((brush, index) => {
-                if(brush.type == 'focus') {
-                    this.chart.updateBrush(index, { start: newVal });
-                    this.chart.render();
-                }
-            });
+        focusStart: {
+            handler(newVal, oldVal) {
+                if(newVal == oldVal) return;
+                this.brushes.forEach((brush, index) => {
+                    if(brush.type == 'focus') {
+                        this.chart.updateBrush(index, { start: newVal });
+                        this.chart.render();
+                    }
+                });
+            },
+            deep: false
         },
-        focusEnd: function(newVal, oldVal) {
-            if(newVal == oldVal) return;
-            this.brushes.forEach((brush, index) => {
-                if(brush.type == 'focus') {
-                    this.chart.updateBrush(index, { end: newVal });
-                    this.chart.render();
-                }
-            });
+        focusEnd: {
+            handler(newVal, oldVal) {
+                if(newVal == oldVal) return;
+                this.brushes.forEach((brush, index) => {
+                    if(brush.type == 'focus') {
+                        this.chart.updateBrush(index, { end: newVal });
+                        this.chart.render();
+                    }
+                });
+            },
+            deep: false
         },
     }
 }
